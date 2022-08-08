3f8c83541693c0dbb900f71553f13248ad9b8457
Maulana Adil <maulanaadil.latif@gmail.com>
2022-08-08 15:23:19 +0700

    working on exercise-2

--- exercise/02-optimistic-ui/app/routes/__app/sales/invoices/$invoiceId.tsx
+++ exercise/02-optimistic-ui/app/routes/__app/sales/invoices/$invoiceId.tsx
@@ -14,6 +14,7 @@ import { requireUser } from "~/session.server";
 import { currencyFormatter, parseDate } from "~/utils";
 import { createDeposit } from "~/models/deposit.server";
 import invariant from "tiny-invariant";
+import { useEffect, useRef } from "react";
 
 export async function loader({ request, params }: LoaderArgs) {
   await requireUser(request);
@@ -152,22 +153,40 @@ export default function InvoiceRoute() {
 function Deposits() {
   const data = useLoaderData<typeof loader>();
   const newDepositFetcher = useFetcher();
-  // 🐨 create a ref for the form (so we can reset it once the submission is finished)
+  const formRef = useRef<HTMLFormElement>(null);
 
-  // 🐨 create a deposits array that includes the user's submission
-  // 💰 you can get the user's submission via newDepositFetcher.submission
-  // 💰 you can convert the depositDate to a Date object via parseDate and then use toLocaleDateString()
+  const deposits = [...data.deposits];
+  if (newDepositFetcher.submission) {
+    const amount = Number(newDepositFetcher.submission.formData.get("amount"));
+    const depositDateVal =
+      newDepositFetcher.submission.formData.get("depositDate");
+    const depositDate =
+      typeof depositDateVal === "string" ? parseDate(depositDateVal) : null;
+    if (
+      !validateAmount(amount) &&
+      depositDate &&
+      !validateDepositDate(depositDate)
+    ) {
+      deposits.push({
+        id: "new",
+        amount,
+        depositDateFormatted: depositDate.toLocaleDateString(),
+      });
+    }
+  }
 
-  // 🐨 add a useEffect that resets the form when the submission is finished
-  // 💰 (newDepositFetcher.state === "idle")
+  useEffect(() => {
+    if (!formRef.current) return;
+    if (newDepositFetcher.state !== "idle") return;
+
+    formRef.current.reset();
+  }, [newDepositFetcher.state]);
 
   return (
     <div>
       <div className="font-bold leading-8">Deposits</div>
-      {/* 🐨 swap this for your optimistic deposits array */}
-      {data.deposits.length > 0 ? (
-        // 🐨 swap this for your optimistic deposits array
-        data.deposits.map((deposit) => (
+      {deposits.length > 0 ? (
+        deposits.map((deposit) => (
           <div key={deposit.id} className={lineItemClassName}>
             <Link
               to={`../../deposits/${deposit.id}`}
@@ -184,7 +203,7 @@ function Deposits() {
       <newDepositFetcher.Form
         method="post"
         className="grid grid-cols-1 gap-x-4 gap-y-2 lg:grid-cols-2"
-        // 🐨 add your form ref here
+        ref={formRef}
       >
         <div className="min-w-[100px]">
           <div className="flex flex-wrap items-center gap-1">
